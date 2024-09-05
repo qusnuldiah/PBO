971e4eabfc676f3a64723ab99251b476d20f8506
qusnuldiah <143980045+qusnuldiah@users.noreply.github.com>
2024-09-05 10:44:38 +0700

    Add files via upload

--- Praktikum02/Percobaan2/Barang.java
+++ Praktikum02/Percobaan2/Barang.java
@@ -1,1 +1,19 @@
+package Percobaan2;
 
+public class Barang {
+    public String kode;
+    public String nama;
+    public double hargaKotor;
+    public double diskon;
+
+    public double getHargaBersih() {
+        return hargaKotor - diskon * hargaKotor;
+    }
+
+    public void displayInfo() {
+        System.out.println("Kode            : " + kode);
+        System.out.println("Nama            : " + nama);
+        System.out.println("Harga Kotor     : " +  hargaKotor);
+        System.out.println("Diskon          : " + getHargaBersih());
+    }
+}
